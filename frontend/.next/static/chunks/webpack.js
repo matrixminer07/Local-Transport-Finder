289e352c1b850e564c153a4799bd0e68663d9f34
matrixminer07 <spandanr098@gmail.com>
2026-01-05 00:41:33 +0530

    Remove old contribute page causing TypeScript error

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "171cc53b91681965"; }
+/******/ 		__webpack_require__.h = function() { return "240c8cbf8ceec6a4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
